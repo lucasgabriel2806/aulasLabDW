95c50d105646dd9691852a8fef82ff9838033fe0
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2026-08-27 09:34:50 -0300

    Adicionar Aula 27/08/2026 - Login

--- Backend/Controllers/UsuarioController.js
+++ Backend/Controllers/UsuarioController.js
@@ -1,5 +1,9 @@
 import Usuario from "./../Models/Usuario.js";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+const JWT_EXPIRATION_MS = 24 * 60 * 60 * 1000;
+const JWT_SECRET = process.env.JWT_SECRET || "sua_chave_secreta_muito_forte";
+import cookieParser from "cookie-parser";
 
 export default class UsuarioController {
     static async Create(req, res) {
@@ -28,4 +32,52 @@ export default class UsuarioController {
         }
 
     } // Fim create
+
+    static async Login(req, res) {
+        
+        const{email, senha} = req.body;
+
+        if(!email || !senha) {
+            return res.status(422).json({message: "Todos os dados são obrigatórios"});
+        }
+
+        try {
+
+            const usuario = await Usuario.findOne({email}).select("+senha");
+
+            if(!usuario)
+            {
+                return res.status(400).json({message: "Credênciais inválidas"})
+            }
+
+            const senhaValida = await argon2.verify(usuario.senha, senha);
+
+            if(!senhaValida)
+            {
+                return res.status(400).json({message: "Credênciais inválidas"})
+            }
+            const tokenPayLoad = {
+                id:usuario._Id,
+                nome:usuario.nome,
+                email:usuario.email
+            };
+            const token = jwt.sign(tokenPayLoad, JWT_SECRET, {expiresIn: '1h'});
+
+            res.cookie("token", token, {
+                httpOnly: true, // Evita acesso por script javascript
+                secure:"false", // Tornar true em produção exige https
+                sameSite:"lax", // Comunicação entre o front e o back
+                maxAge: JWT_EXPIRATION_MS || 3600000 // 1h
+            });
+
+            return res.status(200).json({message: "Login efetuado com sucesso",
+                usuario:{id: usuario.id, nome: usuario.nome, email: usuario.email}, 
+                token
+            });
+
+        } catch (error) {
+            return res.status(500).json({message: "Problema ao efetuar o Login", error});
+        }
+
+    } // Fim login
 }
--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,5 @@
+import "dotenv/config"; // Tem que ser a primeira linha no index.js
+
 // Express é um framework usado para criar o servidor HTTP e a API
 import express from "express";
 
@@ -18,6 +20,11 @@ Ela permite usar o sistema tradicional require() dentro de um projeto que está 
  */
 import { createRequire } from "module";
 
+import cookieParser from "cookie-parser";
+
+const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 // Suporte para importar arquivos json usando ESModules
 const require = createRequire(import.meta.url);
 
@@ -33,12 +40,14 @@ const app = new express();
 // transforme esse JSON em um objeto JavaScript que eu possa acessar."
 app.use(express.json());
 
+app.use(cookieParser());
+
 // Configurando o CORS: Apenas o frontend que estiver rodando em 
 // http://localhost:5173 pode fazer requisições ao backend através do 
 // navegador
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: FRONTEND_URL
 }));
 
 // Obrigatoriamente o swagger deve vir antes das rotas
@@ -50,4 +59,4 @@ app.use("/ToDo", routesTarefa);
 app.use("/ToDo", routesUsuario);
 
 // Iniciando o servidor: Express começar a escutar requisições na porta 5000.
-app.listen(5000);
+app.listen(PORT);
